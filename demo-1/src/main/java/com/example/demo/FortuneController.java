ebbb9d9e25fa11c120c40990086d642ae2fbffc7
santoshkumar <santoshkumar@Santosh-pc>
2019-03-06 10:26:56 -0500

    Initial push of Fortune demo to Github

---
+++ demo-1/src/main/java/com/example/demo/FortuneController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FortuneController {
+
+	//private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(Greeting.class);
+	@RequestMapping("/fort")
+	public @ResponseBody String greet() {
+		 /*LOGGER.trace("doStuff needed more information - {}");
+	        LOGGER.debug("doStuff debug - {}");
+	        LOGGER.info("doStuff info - {}");
+	        LOGGER.warn("doStuff warn - {}");
+	        LOGGER.error("doStuff error - {}");
+		*/
+		System.out.println("Greetings print test!");
+		return "have a good day!";
+	}
+	
+}
